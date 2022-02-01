7a1a5ad478c06aeb3ec33c0a8c4bb9ee8ef30f48
Kauê 地球の土星人 <kauecmf@protonmail.com>
2022-01-31 21:23:50 -0300

    new system

--- database/action.py
+++ database/action.py
@@ -6,61 +6,69 @@ ignore_list = ["data.json", "markdown.md"]
 ignore_format = ["title", "json", "py"]
 image_format = ["jpg", "png", "jpeg"]
 
-def get_title(path):
-    print("getting title for "+path)
+def parse_file(path):
+    #print("getting title for "+path)
     title = path.split(".")
-    print(title)
+    #print(title)
     if (len(title) > 2):
         title = title[:-1]
     title = ".".join(title)
-    print("this")
-    print(title)
+    #print("this")
+    #print(title)
     print("will look for "+title+".title")
     
     if (os.path.isfile(title+".title")):
         with open(title+".title") as file:
-            print(title+".title")
-            return file.read()
+            #print("it who we are")
+            #print(title+".title")
+            dictionary = {}
+            format = file.read().split("\n")
+            for line in format:
+                params = line.split(":")
+                if len(params) >= 2:
+                    if (params[1][0] == " "):
+                        params[1] = params[1][1:]
+                    #print(f"{title}[{params[0].lower()}] = {params[1]}")
+                    dictionary[params[0].lower()] = params[1]
+            print(dictionary)
+            return dictionary
     else:
         return False
 
+def define_json(path, directory, isSub):
+    current_element = parse_file(path)
+    if (current_element):
+        if "type" not in current_element:
+            if directory.split(".")[-1] in image_format:
+                current_element['type'] = 'image'
+            elif isSub:
+                current_element['type'] = 'file'
+            else:
+                current_element['type'] = 'directory'
+        if "title" not in current_element:
+            current_element['title'] = directory
+        if not isSub:
+            current_element['subelements'] = {}
+        return current_element
+    else:
+        return {'type': 'directory', 'subelements':{}}
 
 def loop_through(directory, path):
-    this_object = {directory: {'type': 'directory', 'subelements':{}}};
+    this_object = {directory: define_json(path+directory, directory, False)};
     response = next(os.walk(path+directory), False)
-    print("looping through "+directory)
-    print(response)
-    print(path)
     if response:
-        print("====reponse subdir for "+directory)
-        title = get_title(path+directory)
-        if (title):
-            this_object[directory]['title'] = title
-        else:
-            this_object[directory]['title'] = directory 
         for subdir in response[1]:
-            print("this is a sub dir of "+directory)
-            print(subdir)
             complete_object = loop_through(subdir, path+directory+"/")
             if (path == '.'):
                 with open("./"+subdir+"/data.json","w") as file:
                     file.write(json.dumps({'subelements': complete_object[subdir]['subelements']}, indent=4, sort_keys=True))
             this_object[directory]['subelements'][subdir] = complete_object[subdir]
-        print("====reponse subfile for "+directory)
         for subfile in next(os.walk(path+"/"+directory))[2]:
-            print(subfile)
             if subfile not in ignore_list and subfile.split(".")[-1] not in ignore_format:
-                if subfile.split(".")[-1] in image_format:
-                    this_object[directory]['subelements'][subfile] = {'type':'image'}
-                else:
-                    this_object[directory]['subelements'][subfile] = {'type':'file'}
-                subtitle = get_title(path+directory+"/"+subfile)
-                if (subtitle):
-                    this_object[directory]['subelements'][subfile]['title'] = subtitle
-                else:
-                    this_object[directory]['subelements'][subfile]['title'] = subfile
+                this_object[directory]['subelements'][subfile] = define_json(path+directory+"/"+subfile, subfile, True)
             elif subfile == "markdown.md":
-                this_object[directory]['type'] = 'document'
+                if 'type' not in this_object[directory] or this_object[directory]['type'] == 'directory':
+                    this_object[directory]['type'] = 'document'
     return this_object 
 
 loop_through('','.')
